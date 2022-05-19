91e17e667419991e63e1d93e23543e59be68f07e
7Prince11 <fazzytron@gmail.com>
2022-05-19 10:30:40 +0200

    Last time updated 5/19/2022

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React , {useState, useRef} from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
 //Not using currently
@@ -9,59 +9,65 @@ import './App.css';
 // import InputNewName from './Props/Form/InputNewName';
 // import InputNewJob from './Props/Form/InputNewJob';
 // import InputNewCountry from './Props/Form/InputNewCountry';
+// import InputNewName from './Props/Form/InputNewName';
+// import InputNewCountry from './Props/Form/InputNewCountry';
+// import InputNewJob from './Props/Form/InputNewJob';
+
+
 
 import Posts from './Props/Posts';
 import Form from './Props/Form2.0/Form';
-import InputNewName from './Props/Form/InputNewName';
-import InputNewCountry from './Props/Form/InputNewCountry';
-import InputNewJob from './Props/Form/InputNewJob';
+
 import SelectSort from './UI/Select/SelectSort';
 // import PropsShow from './Props/PropsShow';
- 
- 
+
+
 
 // import Show_Text from './WorkPlace/Show_Text';
 // import Plus_Minus from './WorkPlace/Plus_Minus';
 
 function App() {
-  
-       const [posts , setPosts] = useState([
-         {id : 1 , name : 'Oleksandr' , job : 'Frond-end' , country : 'Ukraine'},
-          {id : 2 , name : 'Olek' , job : 'Middle' , country : 'Ukraine'},
-          {id : 3 , name : 'Alex' , job : 'Backend' , country : 'Ukraine'},
-       ]);
 
- 
-
-         
+  const [posts, setPosts] = useState([
+    { id: 1, name: 'Oleksandr', job: 'Frond-end', country: 'Ukraine' },
+    { id: 2, name: 'Olek', job: 'Middle', country: 'Ukraine' },
+    { id: 3, name: 'Alex', job: 'Backend', country: 'Ukraine' },
+  ]);
 
 
-      
-       const creatForm = (newPost) => {
-          
-        setPosts( [...posts, newPost] );
-
-       }
-
-       const removeForm = (componentToDelete) => {
-
-        setPosts( posts.filter( p=>p.id !== componentToDelete.id ) );
 
 
-         
-       }
 
-       const [selectedSort, setSelectedSort] = useState('');
-       const sortPosts=(sort)=>{
-        setSelectedSort(sort);
-        setPosts([...posts].sort((a,b )=>a[sort].localeCompare(b[sort])));
-      }
-      
-       
+
+
+  const creatForm = (newPost) => {
+
+    setPosts([...posts, newPost]);
+
+  }
+
+  const removeForm = (componentToDelete) => {
+
+    setPosts(posts.filter(p => p.id !== componentToDelete.id));
+
+
+
+  }
+
+  const [selectedSort, setSelectedSort] = useState('');
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+    
+
+  }
+
+
 
   return (
     <div className="App">
-      
+
       {/* <Plus_Minus/> */}
       {/* <Show_Text/> */}
       {/* <Buttons></Buttons>
@@ -69,30 +75,28 @@ function App() {
       <Input></Input> */}
 
 
-      <h2 style={{color : 'white' , fontSize:'35px' , marginBottom: '35px'}}>Form for creating new post</h2>
-      
+      <h2 style={{ color: 'white', fontSize: '35px', marginBottom: '35px' }}>Form for creating new post</h2>
 
 
 
-      <Form create={creatForm} funcform = {removeForm}/>
+
+      <Form create={creatForm} funcform={removeForm} />
       <SelectSort
-      defaultValue = 'Choose by'
-      value=  {selectedSort}
-      onChange={sortPosts}
-    
-      options = {[
-        {value : 'Name' , name : 'Choose by name'},
-        {value : 'Job' , name : 'Choose by job'},
-        {value : 'Country' , name : 'Choose by country'},
+        defaultValue='Choose by'
+        value={selectedSort}
+        onChange={sortPosts}
+
+        options={[
+          { value: 'name', name: 'Choose by name' },
+          { value: 'job', name: 'Choose by job' },
+          { value: 'country', name: 'Choose by country' }
+        ]}
 
 
-      ]}
-      
-      
       />
-      {posts.length !== 0 
-      ? <Posts posts={posts} funcform={removeForm}/> 
-      : <h2 style={{color : 'white' , fontSize:'35px' , marginBottom: '35px'}}>No posts</h2>
+      {posts.length !== 0
+        ? <Posts posts={posts} funcform={removeForm} />
+        : <h2 style={{ color: 'white', fontSize: '35px', marginBottom: '35px' }}>No posts</h2>
       }
       {/* <PropsShow posts=  {{id : 1 , name : 'Oleksandr' , job : 'Frond-end' , country : 'Ukraine'}}/> */}
     </div>
--- src/UI/Select/SelectSort.jsx
+++ src/UI/Select/SelectSort.jsx
@@ -4,15 +4,15 @@ import './SelectedSort.css';
 const SelectSort = ({options , defaultValue , value , onChange}) => {
     return (
         <select value={value}
-        onChange={e => onChange(e.target.value)}
-        
-        > 
-            <option value=''>{defaultValue}</option>
+        onChange={e => onChange(e.target.value)}>
+             
+            <option disabled value=''>{defaultValue}</option>
            
-            {options.map(option => <option 
+            {options.map(option => 
+            <option 
             key={option.value}
             value={option.value}>{option.name}</option>)}
-
+ 
 
         </select>
     );
